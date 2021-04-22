8e94780bfbab7c145beb6bc950af8c243638a1b0
Enrique Gimenez <fenriquegimenez@gmail.com>
2021-04-22 15:04:11 -0400

    nombre cambiado

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { CalcForm } from "./CalcForm";
 function App() {
   return (
     <div className="App">
-      <h1 className="text-center mt-3">Calculadora de comisión</h1>
+      <h1 className="text-center mt-3">Calculadora de comisión inmobiliaria</h1>
       <p className="text-center">
         Por favor ingrese el monto y la cantidad de cuotas totales y ya pagadas.
       </p>
